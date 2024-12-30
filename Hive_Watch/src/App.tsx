30189b89389b42e70edfebc66a97fa32cc1b286c
NK8998 <ne.kioko16@gmail.com>
2024-12-31 00:03:29 +0300

    rename

--- Hive_Watch/src/App.tsx
+++ Hive_Watch/src/App.tsx
@@ -3,8 +3,8 @@ import "./App.css";
 import PageManager from "./routes/page_manager";
 import { useAppDispatch } from "./store/hooks/hooks";
 import { updateWindowWidth } from "./store/app/slice";
-import Masthead from "./shell/masthead/Masthead";
 import Guide from "./shell/guide/guide";
+import Masthead from "./shell/masthead/masthead";
 
 function App() {
   const dispatch = useAppDispatch();
